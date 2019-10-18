ef0a118ae8d95f5a155b06d2087265c9ef8aafb9
Christopher Taglieri <ctaglieri92@gmail.com>
2019-10-18 15:18:36 -0400

    Created basic structure

---
+++ src/chess/Chess.java
@@ -0,0 +1,20 @@
+/**
+ * @author Natalia Bryzhatenko nb631
+ * @author Christopher Taglieri cat197
+ */
+
+package chess;
+
+public class Chess {
+
+	/**
+	 * 
+	 * @param args Main
+	 */
+	public static void main(String[] args) {
+		
+		//Base Point
+		
+	}
+
+}
