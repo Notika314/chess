b6292fda68d0d3e571c229ea5a51e27d30485d39
notika0314 <nb631@scarletmail.rutgers.edu>
2019-10-22 00:27:44 -0400

    change class

--- src/chess/Pawn.java
+++ src/chess/Pawn.java
@@ -1,24 +1,73 @@
-package chess;
+//package chess;
+//
+//public class Pawn extends Piece {
+//
+//	boolean hasMoved;
+//	
+//	public Pawn(int color,int x, int y) {
+//		super(color,'P',x,y);
+//	}
+//	
+//	public void generateValidMoves(Piece board[][]) {
+//		if (hasMoved == false && board[this.xPos][this.yPos+this.color]==null && board[this.xPos][this.yPos+2*this.color] == null) {
+//			this.validMoves[this.xPos][this.yPos+2*this.color] = true;
+//			this.hasValidMove = true;
+//		}
+//		if (this.yPos + this.color < 8 && board[this.xPos][this.yPos+this.color] == null) {
+//			this.validMoves[this.xPos][this.yPos+this.color] = true;
+//			this.hasValidMove = true;
+//		}
+//		
+//		/* generate moves when pawn kills the enemy? */
+//	}
+//	
+//}
+//
+//package chess;
 
 public class Pawn extends Piece {
 
 	boolean hasMoved;
+	boolean passant;
 	
-	public Pawn(int color,int x, int y) {
-		super(color,'p',x,y);
+	public Pawn(int color, int x, int y) {
+		super(color,'P',x,y);
 	}
 	
 	public void generateValidMoves(Piece board[][]) {
-		if (hasMoved == false && board[this.xPos][this.yPos+this.color]==null && board[this.xPos][this.yPos+2*this.color] == null) {
-			this.validMoves[this.xPos][this.yPos+2*this.color] = true;
+		this.hasValidMove = false;
+		if (hasMoved == false && board[this.xPos][this.yPos+1*this.color]==null 
+				&& board[this.xPos][this.yPos+2*this.color] == null) {
+			this.validMoves[this.xPos][this.yPos+(2*this.color)] = true;
 			this.hasValidMove = true;
 		}
-		if (this.yPos + this.color < 8 && board[this.xPos][this.yPos+this.color] == null) {
-			this.validMoves[this.xPos][this.yPos+this.color] = true;
+		if ((this.yPos+(1*this.color) < 8 && this.yPos+(1*this.color) >= 0) 
+				&& board[this.xPos][this.yPos+1*this.color] == null) {
+			this.validMoves[this.xPos][this.yPos+(1*this.color)] = true;
 			this.hasValidMove = true;
 		}
-		
-		/* generate moves when pawn kills the enemy? */
+		//This should handle attacks
+		if ((this.yPos+(1*this.color) < 8 && this.yPos+(1*this.color) >= 0) 
+				&& this.xPos+1 < 8 && board[this.xPos+1][this.yPos+(1*this.color)] != null) {
+			this.validMoves[this.xPos+1][this.yPos+(1*this.color)] = true;
+			this.hasValidMove = true;
+		}
+		if ((this.yPos+(1*this.color) < 8 && this.yPos+(1*this.color) >= 0) 
+				&& this.xPos-1 >= 0 && board[this.xPos-1][this.yPos+(1*this.color)] != null) {
+			this.validMoves[this.xPos-1][this.yPos+(1*this.color)] = true;
+			this.hasValidMove = true;
+		}
+		//I think this will handle en passant valid moves
+		if (this.xPos+1 < 8 && board[this.xPos+1][this.yPos].code.equals("P")
+				&& ((Pawn)board[this.xPos+1][this.yPos]).passant == true) {
+			this.validMoves[this.xPos+1][this.yPos+(1*this.color)] = true;
+			this.hasValidMove = true;
+		}
+		if (this.xPos-1 >= 0 && board[this.xPos-1][this.yPos].code.equals("P")
+				&& ((Pawn)board[this.xPos-1][this.yPos]).passant == true) {
+			this.validMoves[this.xPos-1][this.yPos+(1*this.color)] = true;
+			this.hasValidMove = true;
+		}
 	}
 	
 }
